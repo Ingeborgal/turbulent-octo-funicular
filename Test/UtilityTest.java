470da4cbdb4dbce0aaa72fc4e07a6b8131ffb98f
Ingeborgal <Ingis_al@msn.com>
2015-11-24 16:06:18 +0100

    Halfway done with Utility

--- Test/UtilityTest.java
+++ Test/UtilityTest.java
@@ -4,29 +4,53 @@ import org.junit.Test;
 
 
 public class UtilityTest {
-
-	//	
-	//	@Test
-	//	public void startingTest2(){		
-	//		assertEquals(0, Utility.isEmpty());
-	//		}
+	
 	@Test
-	public void convertFromBinaryToDecimal(){ 
+	public void checkLengthOfString(){
+		
+	}
+
+	@Test
+	public void convertFromBinaryToDecimal_assertEquals(){ 
 		assertEquals(6, Utility.convertFromBinaryToDecimal("110"));
 	}
-
 	@Test (expected = IllegalArgumentException.class)
-	public void convertFromBinaryToDecimal_MaximumLengthForBit_assertEquals(){
+	public void convertFromBinaryToDecimal_exceedsMaximumLengthForBit_assertEquals(){
 		assertEquals(47499036, Utility.convertFromBinaryToDecimal("10110101001100011100011100"));
 	}
-	@Test(expected = IllegalArgumentException.class)
-	public void checkForIllegalArguments(){
+	@Test (expected = IllegalArgumentException.class)
+	public void checkForIllegalArguments_castsException(){
 		Utility.convertFromBinaryToDecimal("65");
 	}
 	@Test
-	public void Empty(){
-		assertEquals(0, Utility.convertFromBinaryToDecimal(""));
+	public void isEmpty_BitString_Returns0_assertEquals(){
+		Utility.convertFromBinaryToDecimal("");
 	}
+	@Test
+	public void convertFromDecimalToBinary(){
+		assertEquals("000000000000000000101101", Utility.convertFromDecimalToBinary(45));
+	}
+	@Test
+	public void convertFromHexToDecimal(){
+		assertEquals(255, Utility.convertFromHexToDecimal("FF"));
+	}
+	@Test (expected = IllegalArgumentException.class)
+	public void convertFromHexToDecimal_exceedsMaximumLengthForHex_throwsIllegalArgumentException(){
+		Utility.convertFromHexToDecimal("123456789");
+	}
+	@Test (expected = IllegalArgumentException.class)
+	public void convertFromHexToDecimal_IllegalDigits_throwsIllegalArgumentException(){
+		Utility.convertFromHexToDecimal("ndf788");
+	}
+	@Test
+	public void convertFromHexToDecimal_emptyString_throwsIllegalArgumentException(){
+		assertEquals(0, Utility.convertFromHexToDecimal(""));
+	}
+	
+//	@Test
+//	public void bitwiseAndOperation(){
+//		
+//	}
 
 
 }
--- src/Utility.java
+++ src/Utility.java
@@ -2,37 +2,72 @@
 public class Utility {
 	
 	private static final int MAX_BIT_LENGTH = 24;
+	private static final int MAX_HEX_LENGTH = 6;
+	private static final String LEGAL_DIGITS_HEX = "0123456789ABCDEF";
 	
 
-	public static int isEmpty() {
-		
+	public static int isEmpty(String string) {
+		if( string.length() == 0 ){
+			return 0;
 	 
-		
-		return length;
+	}
+		return string.length();
 	}
 	
-	public static Object convertFromBinaryToDecimal(String binary ) {
-		controlLegalDigit(binary);
-		char[] numbers = binary.toCharArray();
+	public static int convertFromBinaryToDecimal(String binary ) {
 		int result = 0;
+		if( binary.length() == 0 ){
+			result = 0;
+		}else {
+		controlLegalDigitForBit(binary);
+		char[] numbers = binary.toCharArray();
 		
-
 		for ( int i = numbers.length - 1; i >= 0; i-- )
 			if ( numbers[i] == '1' )
 				result += Math.pow( 2, ( numbers.length - i - 1 ) );
-	
+		}
 		return result;
 		 
 	}
 
 
-	private static void controlLegalDigit(String binary) {
+	private static void controlLegalDigitForBit(String binary) {
 		char [] numbers = binary.toCharArray();
 		
-//		for (int i = 0; i < numbers.length; i++)
 		if (numbers.length > MAX_BIT_LENGTH || !binary.matches("[01]+"))
 			throw new IllegalArgumentException();
+	}
+	
+	private static void controlLegalDigitForHex(String hexadecimalArray){
+		char [] numbers = hexadecimalArray.toCharArray();
 		
+		if(numbers.length > MAX_HEX_LENGTH || !hexadecimalArray.matches("[0123456789ABCDEF]+"))
+			throw new IllegalArgumentException();
+	}
+	
+
+	public static String convertFromDecimalToBinary(int integer) {
+		StringBuilder strBuild = new StringBuilder("");
+		
+		for(int i = 0; i < MAX_BIT_LENGTH; i++, integer /= 2)
+			strBuild.append(integer % 2);
+		
+		return strBuild.reverse().toString();
+	}
+
+	public static int convertFromHexToDecimal(String hexadecimalArray) {
+		
+		hexadecimalArray = hexadecimalArray.toUpperCase();
+		int result = 0;
+		if( hexadecimalArray.length() == 0){
+			result = 0;
+		}else {
+		controlLegalDigitForHex(hexadecimalArray);
+			
+		for( char hexaDecimal : hexadecimalArray.toCharArray() )
+			result = 16 * result + LEGAL_DIGITS_HEX.indexOf(hexaDecimal);
+		}
+		return result;
 	}
 	
 	
