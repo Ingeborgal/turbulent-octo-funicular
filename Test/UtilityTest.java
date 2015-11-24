7b240ed4c8b494b954b4417412dc832bc991d646
Ingeborgal <Ingis_al@msn.com>
2015-11-24 09:59:23 +0100

    Wuhuu

--- Test/UtilityTest.java
+++ Test/UtilityTest.java
@@ -1,31 +1,32 @@
 import static org.junit.Assert.*;
 
-//BitStrings
-//Length of string: 0 - 24, if the string is empty, it should return 0, length of string < 24 => throw illegal argument exception
-//If there is other tokens than 0 & 1, throw illegal argument exception. 
-//you should be able to send in an bit string, and get an int value in return. 
-//you should be able to send in an int value and get a bit string value back, with the length of 24. 
-
-//Hex strings
-//length og string 0 - 6, isEmpty should return 0.
-//is lengt of string more than 6, throw illegal argument exception.
-//if the string contains any other tokens than 0123456789ABCDEF, throw illegal argument exception.
-//
-
 import org.junit.Test;
 
 
 public class UtilityTest {
-	
-	
+
+	//	
+	//	@Test
+	//	public void startingTest2(){		
+	//		assertEquals(0, Utility.isEmpty());
+	//		}
 	@Test
-	public void startingTest2(){		
-		assertEquals(0, Utility.isEmpty());
-		}
-	@Test
-	public void converter(){ 
+	public void convertFromBinaryToDecimal(){ 
 		assertEquals(6, Utility.convertFromBinaryToDecimal("110"));
-		
 	}
-	
+
+	@Test (expected = IllegalArgumentException.class)
+	public void convertFromBinaryToDecimal_MaximumLengthForBit_assertEquals(){
+		assertEquals(47499036, Utility.convertFromBinaryToDecimal("10110101001100011100011100"));
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void checkForIllegalArguments(){
+		Utility.convertFromBinaryToDecimal("65");
+	}
+	@Test
+	public void Empty(){
+		assertEquals(0, Utility.convertFromBinaryToDecimal(""));
+	}
+
+
 }
--- src/Utility.java
+++ src/Utility.java
@@ -1,25 +1,40 @@
 
 public class Utility {
 	
-	private static final int 
-
+	private static final int MAX_BIT_LENGTH = 24;
+	
 
 	public static int isEmpty() {
-	 int length = 0;
+		
+	 
 		
 		return length;
 	}
-
+	
 	public static Object convertFromBinaryToDecimal(String binary ) {
+		controlLegalDigit(binary);
 		char[] numbers = binary.toCharArray();
 		int result = 0;
+		
 
 		for ( int i = numbers.length - 1; i >= 0; i-- )
 			if ( numbers[i] == '1' )
 				result += Math.pow( 2, ( numbers.length - i - 1 ) );
-
+	
 		return result;
+		 
+	}
+
+
+	private static void controlLegalDigit(String binary) {
+		char [] numbers = binary.toCharArray();
+		
+//		for (int i = 0; i < numbers.length; i++)
+		if (numbers.length > MAX_BIT_LENGTH || !binary.matches("[01]+"))
+			throw new IllegalArgumentException();
+		
 	}
 	
-
+	
+	
 }
