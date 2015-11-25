deeb7e873628b233e7deafa04c154a433a0e0b33
Ingeborgal <Ingis_al@msn.com>
2015-11-25 16:01:04 +0100

    DataCollector..

--- Test/DataCollectorTest.java
+++ Test/DataCollectorTest.java
@@ -8,17 +8,14 @@ import static org.mockito.Mockito.*;
 
 
 //Når ei linje med måledata leses, skal følgende skje:
-// Måledata id skal brukes som nøkkel for lagring og gjenfinning av bearbeidede måledata i en passende Java collection klasse
-// Feil antall argumenter skal kaste en exception
+// Feil antall argumenter skal kaste en IllegalArgumentexception
 // Operasjon (bitwise OR eller AND) skal utføres på måledata (bearbeidingen)
 // Originale måledata samt resultat av operasjon (bearbeidede måledata) skal lagres, både som bitstrenger og int verdier, og skal kunne gjenfinnes ved hjelp av måledata id
 // Vi skal logge data med duplikate måledata-id, samt måledata med feil bitoperasjon (alt annet enn 1 eller 2 er å anse som en feil), dette skjer på enkleste måte ved at hele tekstlinja som er lest lagres unna i en liste (en pasende Java Collection klasse)
 public class DataCollectorTest {
-	private Utility util = new Utility();
 	private DataCollector dataCollector;
 	private OpenAndReadFile fileHandler;
-	
-	
+		
 	@Before
 	public void setUp() {
 		fileHandler = mock(OpenAndReadFile.class);
@@ -37,12 +34,40 @@ public class DataCollectorTest {
 		verify(fileHandler, times(1)).nextLine();
 	}
 	@Test
-	public void readMoreFromFile(){
+	public void readMore_CheckIfThereIsMoreToReadInFile_assertTrue(){
 		when(fileHandler.readMore()).thenReturn(true);
 		assertTrue(dataCollector.readMore("file.txt"));
 		verify(fileHandler, times(1)).readMore();
 	}
-	
+	@Test
+	public void getSavedData_getOperation_assertEquals(){
+		String string1 = "100010000010000100000001";
+		String string2 = "100010010010000101001001";
+		dataCollector.saveData("b86d4a 2 " + string1 + " " + string2);
+		assertEquals("2", dataCollector.getSavedData("b86d4a").getOperation());
+	}
+	@Test
+	public void getSavedData_getFirstString_assertEquals(){
+		String string1 = "100010000010000100000001";
+		String string2 = "100010010010000101001001";
+		dataCollector.saveData("b86d4a 2 " + string1 + " " + string2);
+		assertEquals(string1, dataCollector.getSavedData("b86d4a").getFirstString());
+	}
+	@Test
+	public void getSavedData_getSecondString_assertEquals(){
+		String string1 = "100010000010000100000001";
+		String string2 = "100010010010000101001001";
+		dataCollector.saveData("b86d4a 2 " + string1 + " " + string2);
+		assertEquals(string2, dataCollector.getSavedData("b86d4a").getSecondString());
+	}
+		
+	@Test (expected = IllegalArgumentException.class)
+	public void wrongArgument_throwsException(){
+		String string1 = "100010000010000100000001";
+		String string2 = "100010010010000101001001";
+		dataCollector.saveData("b86d4a2 1 " + string1 + " " + string2);
+		
+	}
 
 
 }
--- src/DataCollector.java
+++ src/DataCollector.java
@@ -1,10 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 
 public class DataCollector {
 
 	OpenAndReadFile openAndReadFile;
+	private Map<String, SavedData> saves;
+	private Utility util = new Utility();
 
 	public DataCollector(OpenAndReadFile fileHandler) {
 		this.openAndReadFile = fileHandler;
+		saves = new HashMap<>();
 	}
 
 	public boolean open(String fileName) {
@@ -17,6 +24,54 @@ public class DataCollector {
 
 	public boolean readMore(String string) {
 		return openAndReadFile.readMore();
+	}
+
+	public void saveData(String string) {
+
+		String[] token = splitString(string);
+		saves.put(token[0], new SavedData(token[1], token[2], token[3]));
+
+	}
+
+	private String[] splitString(String string) {
+		StringTokenizer tokenizer = new StringTokenizer(string);
+
+		String[] token = new String[4];
+
+		for ( int i = 0; tokenizer.hasMoreElements(); i++ )
+			token[ i ] = tokenizer.nextToken();
+		if(token.length != 4)
+			throw new IllegalArgumentException();
+		else
+			return token;
+	}
+
+
+	public SavedData getSavedData(String key) {
+		return saves.get(key);
+	}
+
+}
+class SavedData{
+
+	private String operation, firstString, secondString;
+
+	public SavedData(String operation, String firstString, String secondString) {
+		this.operation = operation;
+		this.firstString = firstString;
+		this.secondString = secondString;
+	}
+
+	public String getOperation() {
+		return operation;
+	}
+
+	public Object getFirstString() {
+		return firstString;
+	}
+
+	public Object getSecondString() {
+		return secondString;
 	}
 
 }
